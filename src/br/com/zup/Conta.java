5ac0ae31f54f82b9c652aa55420bbb2b30f9acbf
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-02-23 15:18:09 -0300

    Criação da classe Conta

---
+++ src/br/com/zup/Conta.java
@@ -0,0 +1,54 @@
+package br.com.zup;
+
+public abstract class Conta {
+    private int numero, agencia;
+    private double saldo;
+    private String nome, dataCriacao;
+
+    public Conta(int numero, int agencia, double saldo, String nome) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.saldo = saldo;
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
