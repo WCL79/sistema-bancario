53be2d0dcaeb6198b999def073f11b882e343f69
Ricardo Alcantara Silva <ricardo.alcantara.silva@gmail.com>
2021-02-24 13:55:43 -0300

    Criação da classe Pessoa

---
+++ src/br/com/zup/pessoa/Pessoa.java
@@ -0,0 +1,36 @@
+package br.com.zup.pessoa;
+
+public class Pessoa {
+
+    private String nome, sobrenome, dataNascimento;
+
+    public Pessoa(String nome, String sobrenome, String dataNascimento) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
